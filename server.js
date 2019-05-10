2df19795f30c8cf45f801fa643d13b6e4d0cbe0b
ShinKaiMa <shinkaivin1993@gmail.com>
2019-05-10 12:07:52 +0800

    seiton

--- server.js
+++ server.js
@@ -6,46 +6,25 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 var app = express();
 var DBinitialize = require("./libs/utils/DBInitialize");
-
 var User = require('./models/user');
 
-mongoose.set('useCreateIndex', true);// use createIndex() instead of ensureIndex() in mongodb
+'use strict'
+
+// use createIndex() instead of ensureIndex() in mongodb
+mongoose.set('useCreateIndex', true);
 mongoose.connect(config.database, { useNewUrlParser: true })
-    .then(() => console.log(`Connected to ${config.database} successfully`))
+    .then(() => {
+        console.log(`Connected to ${config.database} successfully`);
+        DBinitialize();
+    })
     .catch((error) => {
         console.log(`Can not connect to ${config.database}, reason: ` + error);
         process.exit(1);
     });
 
-DBinitialize();
-
-
 app.use(express.static(path.join(__dirname, 'dist')))
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-
-// use /setup to create a sample admin user for testing auth apis
-app.get('/setup', function (req, res) {
-
-    bcrypt.hash("password", config.saltRounds).then(function (hash) {
-        // create a sample user
-        var admin = new User({
-            email: 'atmo.io@gmail.com',
-            password: hash,
-            auth: true,
-            admin: true
-        });
-
-        // save the sample user
-        admin.save(function (err) {
-            if (err) throw err;
-        });
-        console.log('User saved successfully');
-        res.json({ success: true });
-    });
-});
-
 
 app.post("/api/login", (req, res) => {
     console.log('get login req: ' + JSON.stringify(req.body));
@@ -58,16 +37,6 @@ app.post("/api/login", (req, res) => {
     });
 })
 
-
 app.listen(5000, () => {
     console.log('running at port 5000');
-    // var token = jwt.sign({ foo: 'bar' }, config.privateKey, { algorithm: 'RS256' });
-    // console.log(token);
-
-    // jwt.verify(token, config.publicKey, { algorithms: ['RS256'] }, function (err, payload) {
-    //     // if token alg != RS256,  err == invalid signature
-    //     console.log('err: ' + err);
-    //     console.log('payload: ' + JSON.stringify(payload));
-    // });
-
 })
