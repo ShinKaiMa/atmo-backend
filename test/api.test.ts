8c1176f1378f9b08276c004f5830521bc58dc4bc
ShinKai <shinkaivin1993@gmail.com>
2020-02-02 20:49:26 +0800

    Add: more detail testing about weathermap API response

--- test/api.test.ts
+++ test/api.test.ts
@@ -2,31 +2,29 @@ import { expect } from 'chai';
 import { app, server } from '../src/server';
 import { agent as request } from 'supertest';
 import mongoose = require('mongoose');
-import { setTimeout } from 'timers';
-
-const sleep = (millsecond) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, millsecond)
-    })
-}
 
 describe('Weathermap API testing', async () => {
-    it('should POST /api/modelView/area', async () => {
-        const res = await request(app)
+    it('should POST /api/modelView/area, and content length is not 0.', async () => {
+        let res = await request(app)
             .post('/api/modelView/area').send({ model: "CWB WRF 3KM" });
         expect(res.status).to.equal(200);
+        expect(res.body).to.not.be.empty;
     });
 
-    it('should POST /api/modelView/schema', async () => {
-        const res = await request(app)
-            .post('/api/modelView/schema').send({ model: "CWB WRF 3KM", area:"TW" });
-        expect(res.status).to.equal(200);
+    it('should POST /api/modelView/schema, and dataTypes keys should equals to 3 (match bottom navbar), startDate must not be empty.', async () => {
+        let areas: string[] = ["TW", "East Asia"]; // fake data
+        for (let idx; idx < areas.length; idx++) {
+            console.log(`idx: ${idx}`)
+            let res = await request(app)
+                .post('/api/modelView/schema').send({ model: "CWB WRF 3KM", area: areas[idx] });
+            expect(res.status).to.equal(200);
+            expect(Object.keys(res.body.dataTypes)).to.have.lengthOf(3);
+            expect(res.body.startDate).to.not.be.empty;
+        }
     });
 
     it('should POST /api/modelView/weathermap', async () => {
-        const res = await request(app)
+        let res = await request(app)
             .post('/api/modelView/weathermap').send({ model: "CWB WRF 3KM", area: "TW", detailType: "Surface Wind and Precip", startDateString: "2020-01-28T18:00:00.000Z" });
         expect(res.status).to.equal(200);
     });
@@ -35,7 +33,6 @@ describe('Weathermap API testing', async () => {
 
 after((done) => {
     server.close(async () => {
-        await sleep(5000);
         await mongoose.connection.close();
         done()
     })
